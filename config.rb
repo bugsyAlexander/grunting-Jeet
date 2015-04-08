c478e01a8b9c17064f2a8e4ff179ca07843438dd
Mynor Alexander <sinrart@yahoo.com>
2015-04-08 16:57:52 -0700

    gruntfile & ruby config file added to project. Used Ruby to get Jeet running

---
+++ config.rb
@@ -0,0 +1,7 @@
+require 'jeet'
+
+css_dir = 'css'
+sass_dir = 'components/sass'
+javascript_dir = 'js'
+# output_style = :compressed
+output_style = :nested
---
+++ gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  grunt.initConfig({
+    uglify: {
+      my_target: {
+        files: {
+          'js/script.js': ['components/js/*.js']
+        } // files
+      } // my_target
+    }, // uglify
+    compass: {
+      dev: {
+        options: {
+          config: 'config.rb'
+        } // options
+      } // dev
+    }, // compass
+    watch: {
+      script: {
+        files: ['components/js/*.js'],
+        tasks: ['uglify']
+      }, // script
+      sass: {
+        files: ['components/sass/*.scss'],
+        tasks: ['compass:dev']
+      } // sass
+    } // watch
+  }); // initConfig
+
+  grunt.registerTask('default', 'watch');
+}; // exports
