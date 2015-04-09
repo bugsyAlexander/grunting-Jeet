9d22bf2125b119c1b7456602a5ab5ab89f26396c
Mynor Alexander <sinrart@yahoo.com>
2015-04-08 17:20:12 -0700

    added jeet vendor files - may need to remove these later. Remove from grunt file tasks aswell if you remove jeet vendor folder

--- gruntfile.js
+++ gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
     uglify: {
       my_target: {
         files: {
-          'js/script.js': ['components/js/*.js']
+          'js/script.js': ['components/js/*.js', 'components/js/vendor/*.js']
         } // files
       } // my_target
     }, // uglify
